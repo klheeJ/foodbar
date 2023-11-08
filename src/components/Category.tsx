6ad5220b61d0dd4cf7c68d323d018eb063fd10a0
klheeJ <115168012+klheeJ@users.noreply.github.com>
2023-11-08 17:11:19 -0600

    Update Category.tsx

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -5,7 +5,7 @@ import { useRef, useEffect, useState } from "react";
 const Category = () => {
 
     const [width, setWidth] = useState(0);
-    const carousel = useRef<HTMLDivElement | null>(null);
+    const carousel = useRef<null | HTMLDivElement>(null);
   
     useEffect(()=>{
         if (carousel.current) {
