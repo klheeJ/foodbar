8dec2827813e7c05a63ce72c7490f37daf05d0be
klheeJ <115168012+klheeJ@users.noreply.github.com>
2023-11-08 17:30:02 -0600

    Update lists.ts

--- src/components/lists.ts
+++ src/components/lists.ts
@@ -1,42 +1,42 @@
 export const featuredProducts = [
     {
-        image: "./public/product1.jpg",
+        image: "/product1.jpg",
         id: "food1",
         name: "pasta with sauce",
         price: "$15.00"
     },
     {
-        image: "./public/product2.jpg",
+        image: "/product2.jpg",
         id: "food2",
         name: "cheeseburger",
         price: "$15.00"
     },
     {
-        image: "./public/product3.jpg",
+        image: "/product3.jpg",
         id: "food3",
         name: "onion pizza",
         price: "$15.00"
     },
     {
-        image: "./public/product4.jpg",
+        image: "/product4.jpg",
         id: "food4",
         name: "loaded burger",
         price: "$15.00"
     },
     {
-        image: "./public/product4.jpg",
+        image: "/product4.jpg",
         id: "food5",
         name: "loaded burger",
         price: "$15.00"
     },
     {
-        image: "./public/product4.jpg",
+        image: "/product4.jpg",
         id: "food6",
         name: "loaded burger",
         price: "$15.00"
     },
     {
-        image: "./public/product4.jpg",
+        image: "/product4.jpg",
         id: "food7",
         name: "loaded burger",
         price: "$15.00"
@@ -46,42 +46,42 @@ export const featuredProducts = [
 
 export const CategoryItems = [
     {
-        image: "./public/product4.jpg",
+        image: "/product4.jpg",
         id: "food1",
         type: "Burger",
     },
     {
-        image: "./public/frenchfries.jpg",
+        image: "/frenchfries.jpg",
         id: "food2",
         type: "French Fries",
     },
     {
-        image: "./public/olivepizza.jpg",
+        image: "/olivepizza.jpg",
         id: "food3",
         type: "Pizza",
     },
     {
-        image: "./public/pasta.jpg",
+        image: "/pasta.jpg",
         id: "food4",
         type: "burger",
     },
     {
-        image: "./public/pasta.jpg",
+        image: "/pasta.jpg",
         id: "food4",
         type: "burger",
     },
     {
-        image: "./public/pasta.jpg",
+        image: "/pasta.jpg",
         id: "food4",
         type: "burger",
     },
     {
-        image: "./public/pasta.jpg",
+        image: "/pasta.jpg",
         id: "food4",
         type: "burger",
     },
     {
-        image: "./public/pasta.jpg",
+        image: "/pasta.jpg",
         id: "food4",
         type: "burger",
     }
@@ -89,84 +89,84 @@ export const CategoryItems = [
 
 export const OfferItems = [
     {
-        image: "./public/offer/1.jpg",
+        image: "/offer/1.jpg",
         id: "food1",
         name: "Bacon Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/2.jpg",
+        image: "/offer/2.jpg",
         id: "food2",
         name: "French Fries",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/3.jpg",
+        image: "/offer/3.jpg",
         id: "food3",
         name: "Bacon Pizza",
         prevPrice: "$20",
         currentPrice: "$18"
     },
     {
-        image: "./public/offer/4.jpg",
+        image: "/offer/4.jpg",
         id: "food4",
         name: "Vegetable Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/5.jpg",
+        image: "/offer/5.jpg",
         id: "food5",
         name: "Cream Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/6.jpg",
+        image: "/offer/6.jpg",
         id: "food6",
         name: "French Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/7.jpg",
+        image: "/offer/7.jpg",
         id: "food7",
         name: "Chicken Pizza",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/8.jpg",
+        image: "/offer/8.jpg",
         id: "food8",
         name: "Tomato Burger",
         prevPrice: "$30",
         currentPrice: "$15"
     },
     {
-        image: "./public/offer/9.jpg",
+        image: "/offer/9.jpg",
         id: "food9",
         name: "Japani Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/10.jpg",
+        image: "/offer/10.jpg",
         id: "food10",
         name: "Tatka French",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/11.jpg",
+        image: "/offer/11.jpg",
         id: "food11",
         name: "Meat Burger",
         prevPrice: "$15",
         currentPrice: "$12"
     },
     {
-        image: "./public/offer/12.jpg",
+        image: "/offer/12.jpg",
         id: "food12",
         name: "Chains Burger",
         prevPrice: "$25",
@@ -178,48 +178,48 @@ export const clientReviews = [
     {
         review: "lorem12",
         name: "Abdur Rajjak",
-        image: "./public/clients/client1.jpg",
+        image: "/clients/client1.jpg",
         id: "client1"
     },
     {
         review: "lorem12",
         name: "Noman Bin",
-        image: "./public/clients/client2.jpg",
+        image: "/clients/client2.jpg",
         id: "client2"
     },
     {
         review: "lorem12",
         name: "Abdur Rajjak",
-        image: "./public/clients/client3.jpg",
+        image: "/clients/client3.jpg",
         id: "client3"
     },
     {
         review: "lorem12",
         name: "Michael Lee",
-        image: "./public/clients/client4.jpg",
+        image: "/clients/client4.jpg",
         id: "client4"
     },
     {
         review: "lorem12",
         name: "Justin Jacobs",
-        image: "./public/clients/client5.jpg",
+        image: "/clients/client5.jpg",
         id: "client5"
     },
 ]
 
 export const foodStories = [
     {
-        image: "./public/stories/1.jpg",
+        image: "/stories/1.jpg",
         desc: "Person Holding A Slice of Pizza",
         id: "1"
     },
     {
-        image: "./public/stories/2.jpg",
+        image: "/stories/2.jpg",
         desc: "Person Holding A Slice of Pizza",
         id: "2"
     },
     {
-        image: "./public/stories/3.jpg",
+        image: "/stories/3.jpg",
         desc: "Person Holding A Slice of Pizza",
         id: "3"
     }
