8946754a84f02f1a8c837772e769061afa2720e7
klheeJ <115168012+klheeJ@users.noreply.github.com>
2023-11-08 22:33:37 -0600

    Update ShopNow.tsx

--- src/components/ShopNow.tsx
+++ src/components/ShopNow.tsx
@@ -2,13 +2,13 @@ const ShopNow = () => {
   return (
     <section className="bg-[#3E3D3D] flex flex-row pt-16 pb-16 justify-center gap-7">
       <div className="shop-parent relative">
-        <img src="./public/chees.jpg" alt="" />
+        <img src="/chees.jpg" alt="" />
         <div className="shop-child absolute inset-0 flex items-center justify-center">
           <button className="shop-button bg-white p-2 pl-10 pr-10 rounded-lg">Shop Now</button>
         </div>
       </div>
       <div className="shop-parent relative">
-        <img src="./public/delicious.jpg" alt="" />
+        <img src="/delicious.jpg" alt="" />
         <div className="shop-child absolute inset-0 flex items-center justify-center">
           <button className="shop-button bg-white p-2 pl-10 pr-10 rounded-lg">Shop Now</button>
         </div>
